68ddb524fb37bc730751b285d124ff206c57e8e5
frokhman <fajar.rokhman@verint.com>
2022-02-09 07:36:40 +0700

    a

--- update.bat
+++ update.bat
@@ -9,15 +9,6 @@ xcopy %bdir%*.js js\ /i /y
 xcopy %bdir%*.d.ts dts /i /y
 
 echo .
-echo ------------
-echo update demo:
-echo ============
-xcopy ..\blijs\demo\collision\web\*.* .\demo\collision /i /y /s
-xcopy ..\blijs\demo\tile\web\*.* .\demo\tile /i /y /s
-xcopy ..\blijs\demo\snow\web\*.* .\demo\snow /i /y /s
-xcopy ..\blijs\demo\expl\web\*.* .\demo\expl /i /y /s
-
-echo .
 echo ------------------
 echo update playground:
 echo ==================
