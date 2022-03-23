e13f808e28cde1c60e226afd9a302e97d3680205
frokhman <fajar.rokhman@verint.com>
2022-03-23 13:03:46 +0700

    jenkins

--- script.groovy
+++ script.groovy
@@ -2,7 +2,7 @@ def buildApp() {
     echo "building the application ..."
 }
 
-@Grab('com.thycotic:devops-secrets-vault-sdk:1.0.1');
+@Grab('com.thycotic:devops-secrets-vault-sdk:1.0.1')
 import org.apache.commons.math3.primes.Primes
 void parallelize(int count) {
   if (!Primes.isPrime(count)) {
