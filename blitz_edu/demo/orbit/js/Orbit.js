d9e6b600f5e75fe63ba60f50055665d42437cb29
fajar <rokhman.fajar@gmail.com>
2022-11-17 06:25:15 +0700

    a

--- blitz_edu/demo/orbit/js/Orbit.js
+++ blitz_edu/demo/orbit/js/Orbit.js
@@ -4,23 +4,23 @@ let bumi;
 let sudut = 10;
 //START PROGRAM
 async function Mulai() {
-    Grafis(480, 480);
-    matahari = await MuatSprite("./gbr/matahari.png", true);
-    UkuranSprite(matahari, 100, 100);
-    HandleSpriteTengah(matahari);
-    PosisiSprite(matahari, 240, 240);
-    bumi = await MuatSprite("./gbr/bumi.png", true);
-    UkuranSprite(bumi, 50, 50);
-    HandleSpriteTengah(bumi);
-    PosisiSprite(bumi, 500, 240);
+	Grafis(480, 480);
+	matahari = await MuatSprite("./gbr/matahari.png", true);
+	UkuranSprite(matahari, 100, 100);
+	HandleSpriteTengah(matahari);
+	PosisiSprite(matahari, 240, 240);
+	bumi = await MuatSprite("./gbr/bumi.png");
+	UkuranSprite(bumi, 50, 50);
+	HandleSpriteTengah(bumi);
+	PosisiSprite(bumi, 500, 240);
 }
 //LOOOP
 async function Loop() {
-    Bersih();
-    sudut++;
-    if (sudut > 360) {
-        sudut -= 360;
-    }
-    PosisiPolarSprite(bumi, sudut, 160, PosisiXSprite(matahari), PosisiYSprite(matahari));
-    TaruhSemuaSprite();
+	Bersih();
+	sudut++;
+	if (sudut > 360) {
+		sudut -= 360;
+	}
+	PosisiPolarSprite(bumi, sudut, 160, PosisiXSprite(matahari), PosisiYSprite(matahari));
+	TaruhSemuaSprite();
 }
