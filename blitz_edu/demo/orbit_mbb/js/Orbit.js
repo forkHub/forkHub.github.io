cf4d2fb7cd16e4fcd9afc9c26a6c8e271e764a00
frokhman <fajar.rokhman@verint.com>
2022-11-20 08:46:54 +0700

    a

--- blitz_edu/demo/orbit_mbb/js/Orbit.js
+++ blitz_edu/demo/orbit_mbb/js/Orbit.js
@@ -1,35 +1,31 @@
-//DECLARE GLOBAL VARIABLE
+Grafis(480, 480);
 let matahari;
 let bumi;
 let sudut = 10;
 let bulan;
 let sudutBulan = 0;
-//START PROGRAM
-async function Mulai() {
-	Grafis(480, 480);
-	matahari = await MuatSprite("./gbr/matahari.png", true);
-	UkuranSprite(matahari, 100, 100);
-	HandleSpriteTengah(matahari);
-	PosisiSprite(matahari, 240, 240);
-	bumi = await MuatSprite("./gbr/bumi.png");
-	UkuranSprite(bumi, 50, 50);
-	HandleSpriteTengah(bumi);
-	bulan = await MuatSprite("./gbr/bulan_32.png");
-	UkuranSprite(bulan, 32, 32);
-	HandleSpriteTengah(bulan);
-}
+matahari = Muat("./gbr/matahari.png", true);
+Ukuran(matahari, 100, 100);
+Handle(matahari, 50, 50);
+Posisi(matahari, 240, 240);
+bumi = Muat("./gbr/bumi.png");
+Handle(bumi, 25, 25);
+Ukuran(bumi, 50, 50);
+bulan = Muat("./gbr/bulan_32.png");
+Ukuran(bulan, 32, 32);
+Handle(bulan, 16, 16);
 //LOOOP
-async function Loop() {
-	Bersih();
-	sudut += .5;
-	if (sudut > 360) {
-		sudut -= 360;
-	}
-	sudutBulan += 6;
-	if (sudutBulan > 360) {
-		sudutBulan -= 360;
-	}
-	PosisiPolarSprite(bumi, sudut, 160, PosisiXSprite(matahari), PosisiYSprite(matahari));
-	PosisiPolarSprite(bulan, sudutBulan, 80, PosisiXSprite(bumi), PosisiYSprite(bumi));
-	TaruhSemuaSprite();
+function Loop() {
+    Bersih();
+    sudut += .5;
+    if (sudut > 360) {
+        sudut -= 360;
+    }
+    sudutBulan += 6;
+    if (sudutBulan > 360) {
+        sudutBulan -= 360;
+    }
+    PosisiPolar(bumi, sudut, 160, PosisiX(matahari), PosisiY(matahari));
+    PosisiPolar(bulan, sudutBulan, 80, PosisiX(bumi), PosisiY(bumi));
+    GambarSemua();
 }
