11b5fcf336e784ba521d572319948e0de680eeb1
fajar <rokhman.fajar@gmail.com>
2022-11-22 08:25:50 +0700

    a

--- blitz_edu/demo/drag02/js/Drag.js
+++ blitz_edu/demo/drag02/js/Drag.js
@@ -1,6 +1,10 @@
 Grafis(240, 320);
+
+
+
 let spr;
 let spr2;
+
 spr = Muat("./gbr/box.png", true);
 Posisi(spr, 160, 120);
 spr2 = Muat("./gbr/box.png", true);
@@ -8,6 +12,6 @@ Posisi(spr2, 160, 120);
 console.log('test');
 let stat = false;
 function Loop() {
-    Bersih();
-    GambarSemua();
+	Bersih();
+	GambarSemua();
 }
