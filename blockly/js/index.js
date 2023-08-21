815fa1b3e3eab3ae88ad6344d944cd42bd2fdcf6
frokhman <fajar.rokhman@verint.com>
2023-08-21 14:25:44 +0700

    a

--- blockly/js/index.js
+++ blockly/js/index.js
@@ -803,7 +803,7 @@ var ha;
         var debugData;
         (function (debugData) {
             debugData.list = [];
-            debugData.group = "Debug1";
+            debugData.group = "Debug2";
             debugData.list.push({
                 type: 'ha.bbjs.Debug.Obj',
                 message0: "Debug %1 ",
