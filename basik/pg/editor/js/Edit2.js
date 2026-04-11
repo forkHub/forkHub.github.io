ea0bdfaac8e994d0e353043ccfa2241fe0493715
fajar <rokhman.fajar@gmail.com>
2026-04-11 16:51:48 +0700

    update v02

--- basik/pg/editor/js/Edit2.js
+++ basik/pg/editor/js/Edit2.js
@@ -1,159 +1,238 @@
 "use strict";
+var EState;
+(function (EState) {
+    EState[EState["awal"] = 0] = "awal";
+    EState[EState["edit"] = 1] = "edit";
+    EState[EState["dialogSimpan"] = 2] = "dialogSimpan";
+    EState[EState["jalankan"] = 3] = "jalankan";
+})(EState || (EState = {}));
+//globa state
+let fileDiedit = false;
+let fileHash = '';
+let fileNama = "latihan_" + (Math.floor(Math.random() * 1000) + 1000);
+let fileList = [];
+let fileBaru = true;
+let fileAktif = {
+    namaFile: fileNama,
+    data: ""
+};
+let appState = EState.edit;
+const storageNama = "io.github.forkhub.basik.data";
 class Edit2 {
     constructor() {
         this.editArea = document.querySelector('textarea.edit-area');
         this.webCont = document.querySelector('div.kontainer-2 div.web');
         this.editCont = document.querySelector('div.kontainer-2 div.edit-text');
-        this.tblRun = document.querySelector('button.jalan');
-        this.tblEdit = document.querySelector('button.edit');
-        this.tblSimpan = document.querySelector('button.simpan');
-        this.tblMuat = document.querySelector('button.muat');
-        this.tblUnduh = document.querySelector('button.unduh');
-        this.tblUnggah = document.querySelector('button.unggah');
+        this.tblEditGroup = ["simpan", "muat", "jalan", "baru"];
+        this.tblJalanGroup = ["edit",];
+        this.webCont;
+        this.editCont;
+        this.fileInfo = document.querySelector("div.file-info span.nama");
+    }
+    getTbl(nama) {
+        return document.querySelector(`button.${nama}`);
+    }
+    showTbl(tbl) {
+        if (Array.isArray(tbl)) {
+            tbl.forEach((item) => {
+                this.showTbl(item);
+            });
+        }
+        else if (typeof tbl === "string") {
+            this.showTbl(this.getTbl(tbl));
+        }
+        else {
+            tbl.classList.add('disp-inline');
+            tbl.classList.remove('disp-none');
+        }
+    }
+    hideTbl(tbl) {
+        if (Array.isArray(tbl)) {
+            tbl.forEach((item) => {
+                this.hideTbl(item);
+            });
+        }
+        else if (typeof tbl == "string") {
+            this.hideTbl(this.getTbl(tbl));
+        }
+        else {
+            tbl.classList.remove('active');
+            tbl.classList.remove('disp-inline');
+            tbl.classList.add('disp-none');
+        }
     }
     init() {
-        this.tblSimpan.onclick = () => {
-            // let item: Item = {
-            // 	id: Date.now() + '',
-            // 	name: ''
-            // }
-            // //TODO:
-            // item;
-            // localStorage.set("");
-            // CodeMirror.getValue();
-            dlgBelumSelesai();
+        this.getTbl("simpan").onclick = () => {
+            this.simpanKlik();
         };
-        this.tblMuat.onclick = () => {
-            dlgBelumSelesai();
-            /*
-            let item: Item[] = [];
-
-            //load from db
-            try {
-                let itemStr = localStorage.getItem("basik_save");
-                item = JSON.parse(itemStr) || [];
-            }
-            catch (e) {
-                console.error(e);
-                item = [];
-            }
-
-            console.log(item);
-
-            dialogListFile(item, (selectedItem: Item | null) => {
-                console.log(selectedItem);
-            }, () => {
-                console.log("closed");
-            })
-                */
+        this.getTbl("muat").onclick = () => {
+            this.muatKlik();
         };
-        this.tblUnduh.onclick = () => {
-            dlgBelumSelesai();
-        };
-        this.tblUnggah.onclick = () => {
-            dlgBelumSelesai();
-        };
-        this.tblRun.onclick = () => {
+        this.getTbl("jalan").onclick = () => {
             console.debug('run');
             this.editCont.classList.remove('active');
-            this.tblEdit.classList.remove('active');
-            this.tblEdit.classList.add('disp-inline');
-            this.tblMuat.classList.add('disp-none');
-            this.tblSimpan.classList.add('disp-none');
-            this.tblUnduh.classList.add('disp-none');
-            this.tblUnggah.classList.add('disp-none');
-            this.tblMuat.classList.remove('disp-inline');
-            this.tblSimpan.classList.remove('disp-inline');
-            this.tblUnduh.classList.remove('disp-inline');
-            this.tblUnggah.classList.remove('disp-inline');
-            this.tblRun.classList.add('active');
-            this.tblRun.classList.add('disp-none');
-            this.tblRun.classList.remove('disp-inline');
             this.webCont.classList.add('active');
+            this.hideTbl(this.tblEditGroup);
+            this.showTbl(this.tblJalanGroup);
+            // this.tblEdit.classList.remove('active');
+            // this.tblEdit.classList.add('disp-inline');
+            // this.tblMuat.classList.add('disp-none');
+            // this.tblSimpan.classList.add('disp-none');
+            // this.tblMuat.classList.remove('disp-inline');
+            // this.tblSimpan.classList.remove('disp-inline');
+            // this.tblRun.classList.add('active');
+            // this.tblRun.classList.add('disp-none');
+            // this.tblRun.classList.remove('disp-inline');
             this.compile();
         };
-        this.tblEdit.onclick = () => {
+        this.getTbl("edit").onclick = () => {
             this.editClick();
         };
-        // const eslintConfig = {
-        // 	parserOptions: {
-        // 		ecmaVersion: 2015,
-        // 		sourceType: "module"
-        // 	},
-        // 	env: {
-        // 		es6: true,
-        // 		browser: true
-        // 	},
-        // 	rules: {
-        // 		semi: ["error", "always"],
-        // 		"no-unused-vars": "warn"
-        // 	}
-        // };
-        // CodeMirror.registerHelper("lint", "javascript", function (text: any) {
-        // 	const results = ESLint.verify(text, eslintConfig);
-        // 	return results.map((result: any) => ({
-        // 		from: CodeMirror.Pos(result.line - 1, result.column - 1),
-        // 		to: CodeMirror.Pos(result.line - 1, result.column),
-        // 		message: result.message,
-        // 		severity: result.severity === 2 ? "error" : "warning"
-        // 	}));
-        // });
+        this.getTbl("baru").onclick = () => {
+            this.baruKlik();
+        };
+        // this.getTbl("demo").onclick = () => {
+        // 	this.demoKlik();
+        // }
         this.myCodeMirror = CodeMirror.fromTextArea(this.editArea, {
             lineNumbers: true,
             mode: "javascript",
             gutters: ["CodeMirror-lint-markers"],
             lint: false
         });
-        // console.log(CodeMirror);
-        // console.log(this.myCodeMirror);
         this.myCodeMirror.on("change", () => {
             // console.log('change');
+            this.updateNama();
         });
-        // this.loadFromQuery();
+        this.hideTbl("edit");
+        this.muatFileAwal();
+        this.fileInfo.innerText = fileNama;
     }
-    // loadFromQuery(): void {
-    // 	try {
-    // 		//loading
-    // 		let s: string = window.top.location.search.slice(1);
-    // 		console.log('url: ' + s);
-    // 		let ar: string[] = s.split('&');
-    // 		console.log(ar);
-    // 		ar = ar[0].split('=');
-    // 		console.log(ar);
-    // 		console.log('loading: ' + ha.comp.loading);
-    // 		ha.comp.Util.Ajax2('get', "./data/" + ar[1] + ".js", '').then((value: string) => {
-    // 			this.myCodeMirror.setValue(value);
-    // 			// this.compile();
-    // 		}).catch((e) => {
-    // 			console.error(e);
-    // 			ha.comp.dialog.tampil('Colud not load data');
-    // 		});
-    // 		//load query
-    // 		console.log(ar);
-    // 	}
-    // 	catch (e) {
-    // 		console.error(e);
-    // 	}
-    // }
-    // klikRun(): void {
-    // 	this.compile();
-    // }
+    demoKlik() {
+        let demo = document.querySelector("dialog.demo") || (() => { throw new Error("Demo dialog not found"); })();
+        demo.showModal();
+    }
+    baruKlik() {
+        window.location.reload();
+    }
+    muatKlik() {
+        dialogDaftarFile((item) => {
+            console.log("muat data");
+            console.log(item.data);
+            fileBaru = false;
+            fileNama = item.namaFile;
+            fileAktif = item;
+            this.myCodeMirror.setValue(item.data);
+            this.updateNama();
+        }, () => {
+            //nothing
+        });
+    }
+    muatFileAwal() {
+        console.log("muat file awal:");
+        try {
+            let s = window.localStorage.getItem(storageNama);
+            fileList = JSON.parse(s);
+            if (!fileList)
+                fileList = [];
+            console.log(fileList);
+        }
+        catch (e) {
+            console.warn(e);
+            fileList = [];
+        }
+    }
     editClick() {
+        // this.gantiState(EState.edit);
         this.editCont.classList.add('active');
         this.editArea.classList.add('active');
-        this.tblRun.classList.remove('active');
-        this.tblRun.classList.add('disp-inline');
-        this.tblRun.classList.remove('disp-none');
+        // this.tblRun.classList.remove('active');
+        // this.showTbl(this.getTbl("jalan"));
+        // this.tblRun.classList.add('disp-inline');
+        // this.tblRun.classList.remove('disp-none');
         this.webCont.classList.remove('active');
         this.webCont.innerHTML = '';
-        this.tblEdit.classList.add('active');
-        this.tblEdit.classList.add('disp-none');
-        this.tblEdit.classList.remove('disp-inline');
-        this.tblSimpan.classList.add('disp-inline');
-        this.tblMuat.classList.add('disp-inline');
-        this.tblUnduh.classList.add('disp-inline');
-        this.tblUnggah.classList.add('disp-inline');
+        // this.tblEdit.classList.add('active');
+        // this.tblEdit.classList.add('disp-none');
+        // this.tblEdit.classList.remove('disp-inline');
+        // this.tblSimpan.classList.add('disp-inline');
+        // this.tblMuat.classList.add('disp-inline');
+        // this.tblEditGroup.forEach((item) => {
+        // 	this.showTbl(this.getTbl(item));
+        // })
+        this.showTbl(this.tblEditGroup);
+        this.hideTbl(this.tblJalanGroup);
         console.log('edit click');
+    }
+    simpanKlik() {
+        let dataKode = this.myCodeMirror.getValue();
+        if (!dataKode) {
+            alert("Tidak ada data yang disimpan, Anda belum menulis apa-apa.");
+            return;
+        }
+        let nama = prompt("Nama file:", fileNama);
+        if (!nama) {
+            return;
+        }
+        let self = this;
+        let gantiNama = (nama != fileNama);
+        if (fileBaru) {
+            resolveKonflik();
+        }
+        else {
+            if (gantiNama) {
+                resolveKonflik();
+            }
+            else {
+                timpa();
+            }
+        }
+        this.updateNama();
+        function resolveKonflik() {
+            if (checkKonflik()) {
+                if (confirm("Nama file sudah ada, apakah mau di timpa?")) {
+                    timpa();
+                    fileBaru = false;
+                    fileNama = nama;
+                }
+                else {
+                    //batal simpan
+                }
+            }
+            else {
+                simpanBaru();
+            }
+        }
+        function checkKonflik() {
+            let konflik = false;
+            for (let i = 0; i < fileList.length; i++) {
+                if (fileList[i].namaFile === nama) {
+                    konflik = true;
+                }
+            }
+            return konflik;
+        }
+        function timpa() {
+            fileList.forEach((item) => {
+                if (item.namaFile == nama) {
+                    item.data = dataKode;
+                    fileAktif = item;
+                }
+            });
+            window.localStorage.setItem(storageNama, JSON.stringify(fileList));
+            fileBaru = false;
+            fileNama = nama;
+        }
+        function simpanBaru() {
+            fileAktif = {
+                namaFile: nama,
+                data: self.myCodeMirror.getValue()
+            };
+            fileList.push(fileAktif);
+            fileBaru = false;
+            fileNama = nama;
+            window.localStorage.setItem(storageNama, JSON.stringify(fileList));
+        }
     }
     compile() {
         JSHINT('/* jshint esversion: 6 */\n' +
@@ -171,6 +250,7 @@ class Edit2 {
                 this.runOk();
             }, () => {
                 this.editClick();
+                // this.gantiState(EState.edit);
             });
         }
         else {
@@ -181,7 +261,7 @@ class Edit2 {
         // console.log(hal2);
     }
     runOk() {
-        let hal2 = render(this.myCodeMirror.getValue());
+        let hal2 = renderIframe(this.myCodeMirror.getValue());
         // hal2 = hal2.replace('{{script}}', this.myCodeMirror.getValue());
         let iframe = document.createElement('iframe');
         let iframeCont = document.body.querySelector('div.kontainer-2 div.web');
@@ -193,6 +273,19 @@ class Edit2 {
             iframe.contentWindow.document.close();
         }, 0);
     }
+    checkFileUpdated() {
+        let data = this.myCodeMirror.getValue();
+        let fData = fileAktif.data;
+        return (data != fData);
+    }
+    updateNama() {
+        if (this.checkFileUpdated()) {
+            this.fileInfo.innerText = fileNama + "(*)";
+        }
+        else {
+            this.fileInfo.innerText = fileNama;
+        }
+    }
 }
 window.onload = () => {
     let edit = new Edit2();
@@ -202,7 +295,8 @@ window.onload = () => {
     // JSHINT.warnings = jshintWarnings;
     // JSHINT.info = jsHintInfo;
 };
-function render(script) {
+function renderIframe(script) {
+    let rand = Math.floor(Math.random() * 1000);
     let hal = `
 <html>
 <meta charset="utf-8" />
@@ -220,6 +314,40 @@ function render(script) {
 			padding: 0px;
 			background-color: lightGray;
 		}
+
+		.custom-alert-overlay {		
+			position: fixed;
+			top: 0;
+			left: 0;
+			width: 100%;
+			height: 100%;
+			background: rgba(0, 0, 0, 0.5);
+			display: none;
+			/* awalnya hidden */
+			justify-content: center;
+			align-items: center;
+			z-index: 9999;
+		}
+
+		.custom-alert-box {
+			background: #fff;
+			padding: 20px;
+			border-radius: 8px;
+			min-width: 250px;
+			text-align: center;
+			box-shadow: 0 4px 10px rgba(0, 0, 0, 0.3);
+		}
+
+		.custom-alert-box button {
+			margin-top: 15px;
+			padding: 8px 16px;
+			border: none;
+			background: #0078d7;
+			color: #fff;
+			border-radius: 4px;
+			cursor: pointer;
+		}
+
 	</style>
 	<script>
 	function loadScriptsSequentially(sources, finalCallback) {
@@ -244,8 +372,8 @@ function render(script) {
 
 	// Example usage:
 	loadScriptsSequentially([
-		'./editor/lib/basik.js',
-		'./editor/lib/mulai.js'
+		'./editor/lib/basik.min.js?r=${rand}',
+		'./editor/lib/mulai.js?r=${rand}'
 	], () => {
 		const script = document.createElement('script');
 		script.textContent = \`${script}\`
--- basik/update.bat
+++
@@ -1,7 +0,0 @@
-rem copy web
-xcopy ..\web . /s /i /f /y
-xcopy ..\playground\web .\pg /s /i /f /y
-xcopy ..\doc .\doc /s /i /f /y
-
-pause
-pause
